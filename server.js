363b549818e3a84c1e2222957bc81ba6772afaa0
Farid Eyvazov <ferideyvazov25@gmail.com>
2021-08-24 13:13:58 +0400

    server side completed

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit:'30mb', extended:true}));
+app.use(bodyParser.urlencoded({ limit:'30mb', extended:true}));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+//Routes
+app.use('/posts', postRoutes);
+
+
+// Connect to Database
+// mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true, useUnifiedTopology:true})
+//     .then(()=>app.listen(PORT,()=>console.log(`Server running at http://localhost:${PORT}`)))
+//     .catch(err=>console.log(err.message))
+
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true, useUnifiedTopology:true},() => {
+    try {
+        console.log("Connected to DB");
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+//start on server
+app.listen(process.env.PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+})
